bbf328a10c252398678b6c1524cb1fd840f38c69
goeun9795 <jgn9795@gmail.com>
2021-07-30 03:33:02 +0900

    Init

--- app/src/main/java/com/android/goods_market/MainActivity.kt
+++ app/src/main/java/com/android/goods_market/MainActivity.kt
@@ -20,6 +20,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var myPageBtn : ImageView
     lateinit var mainBoxImage1 : ImageView
     lateinit var mainBoxText1 : TextView
+    lateinit var mainBoxImage2 : ImageView
+    lateinit var mainBoxText2 : TextView
+    lateinit var mainBoxImage3 : ImageView
+    lateinit var mainBoxText3 : TextView
+    lateinit var mainBoxImage4 : ImageView
+    lateinit var mainBoxText4 : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,6 +39,12 @@ class MainActivity : AppCompatActivity() {
         myPageBtn = findViewById<ImageView>(R.id.myPageBtn)
         mainBoxImage1 = findViewById<ImageView>(R.id.mainBoxImage1)
         mainBoxText1 = findViewById<TextView>(R.id.mainBoxText1)
+        mainBoxImage2 = findViewById<ImageView>(R.id.mainBoxImage2)
+        mainBoxText2 = findViewById<TextView>(R.id.mainBoxText2)
+        mainBoxImage3 = findViewById<ImageView>(R.id.mainBoxImage3)
+        mainBoxText3 = findViewById<TextView>(R.id.mainBoxText3)
+        mainBoxImage4 = findViewById<ImageView>(R.id.mainBoxImage4)
+        mainBoxText4 = findViewById<TextView>(R.id.mainBoxText4)
         menuBtn.setOnClickListener {
             drawer.openDrawer(GravityCompat.START)
         }
@@ -56,6 +69,24 @@ class MainActivity : AppCompatActivity() {
         mainBoxText1.setOnClickListener {
             var intent = Intent(this, PurchaseActivity::class.java)
             startActivity(intent)
+        }
+        mainBoxImage2.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
+        }
+        mainBoxText2.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
+        }
+        mainBoxImage3.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
+        }
+        mainBoxText3.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
+        }
+        mainBoxImage4.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
+        }
+        mainBoxText4.setOnClickListener {
+            Toast.makeText(this,"판매 완료된 상풉입니다.", Toast.LENGTH_SHORT).show()
         }
 
     }
